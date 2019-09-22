670be5bdb0afb9696935ac5dcdc777cc94a79469
Karoline Ikenami <uskyjp@gmail.com>
2019-09-22 18:24:13 -0300

    :zap: pingu first steps! ~

--- src/features/chitchat.ts
+++ src/features/chitchat.ts
@@ -2,8 +2,20 @@ import { App } from "@slack/bolt";
 
 export class Chitchat {
   constructor(app: App) {
-    app.message('does it work?', ({ message, say }) => {
-      say(`We hope so, <@${message.user}>\!`);
+    app.message(/^(hi+|he+llo+|he+y+|o+i+|e a(í|i)+)*/, ({ message, say }) => {
+      say(`Sup <@${message.user}> ヽ(・∀・)ﾉ howdy?`);
+    });
+
+    app.message(/^ping*/, ({ message, say }) => {
+      say(`Tá de brimks comigo, <@${message.user}>??? (」°ロ°)」`);
+      say(`.............. pong`);
+    });
+
+    app.message(/^tem hora?*/, ({ message, say }) => {
+      const now = new Date().toISOString();
+
+      say(`Tenho sim: ${now}`);
+      say(`Mas e você? (￢‿￢ )`);
     });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -15,14 +15,11 @@ const app = new App({
     receiver: expressReceiver
 });
 
-app.message('hello', ({ message, say }) => {
-    say(`Hey there <@${message.user}>\!`);
-});
-
+// import all features here
 new Chitchat(app);
 
 (async () => {
     await app.start(process.env.PORT || 3000);
 
-    console.log('⚡️ Bolt app is running!');
+    console.log('Bwahahahaha, Pingu is alive (✧ω✧)');
 })();
