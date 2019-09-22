823cb1d42b175f3942cb2216703cc32e0dfb44dc
Karoline Ikenami <uskyjp@gmail.com>
2019-09-22 18:47:55 -0300

    :lipstick: quality of life

--- src/features/chitchat.ts
+++ src/features/chitchat.ts
@@ -2,20 +2,26 @@ import { App } from "@slack/bolt";
 
 export class Chitchat {
   constructor(app: App) {
-    app.message(/^(hi+|he+llo+|he+y+|o+i+|e a(í|i)+)*/, ({ message, say }) => {
+    app.message(/^(hi+|he+llo+|he+y+|o+i+|e a(í|i)+).*/, ({ message, say }) => {
       say(`Sup <@${message.user}> ヽ(・∀・)ﾉ howdy?`);
     });
 
-    app.message(/^ping*/, ({ message, say }) => {
+    app.message(/^ping.*/, ({ message, say }) => {
       say(`Tá de brimks comigo, <@${message.user}>??? (」°ロ°)」`);
       say(`.............. pong`);
     });
 
-    app.message(/^tem hora\?*/, ({ message, say }) => {
-      const now = new Date().toISOString();
+    app.message(/^tem hora\?.*/, ({ message, say }) => {
+      const now = new Date();
 
       say(`Tenho sim: ${now}`);
       say(`Mas e você? (￢‿￢ )`);
+    });
+
+    app.message(/^echo .+/, ({ context, say }) => {
+      const echoing = context.matches[1];
+
+      say(echoing);
     });
   }
 }
